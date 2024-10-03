2a83886699ca58a4974f491aa31210a9bd3d7fb6
charlesLoder <charles.w.loder@gmail.com>
2024-10-03 15:10:49 -0400

    Add page meta

--- views/layout/base.js
+++ views/layout/base.js
@@ -18,6 +18,9 @@ const Base = (context, content) => {
       <head>
         <title>Library</title>
         <link rel="stylesheet" href="/public/style.css" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta charset="UTF-8" />
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📚</text></svg>">
       </head>
       <body class="stack">
         ${Header(context)}
